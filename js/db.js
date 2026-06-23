70f96857ac39c6eb9bcfead01b339a5c184362c9
spmarcelo1409 <mgm140902@gmail.com>
2026-06-22 20:02:29 -0600

    Se hizo una reestructuracion total para la aplicacion mejorando index y db .js en el cual se añade tambien pedidos.htmls

--- js/db.js
+++ js/db.js
@@ -1,5 +1,55 @@
 db.collection("platillos").onSnapshot((coleccion) => {
-    coleccion.forEach((registro) => {
-        mostrarPlatillo(registro.data(), registro.id);
+    coleccion.docChanges().forEach((registro) => {
+        if (registro.type === "added"){
+            mostrarPlatillo(registro.doc.data(), registro.doc.id);
+            agregarALista(registro.doc.data(), registro.doc.id);
+        }
+        if (registro.type === "modified"){
+            actualizarPlatillo(registro.doc.data(), registro.doc.id);
+        }
+        if (registro.type === "removed"){
+            borrarPlatillo(registro.doc.id);
+        }
     });
+});
+
+const formularioAgregar = document.querySelector("form");
+formularioAgregar.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const platilloNuevo = {
+        nombre: formularioAgregar.title.value,
+        ingredientes: formularioAgregar.ingredientes.value,
+        precio: formularioAgregar.price.value
+    }
+    db.collection("platillos").add(platilloNuevo)
+    .catch((error) => {
+        console.log(error);
+        alert("Error al agregar platillo");
+    }
+    );
+
+    formularioAgregar.title.value = "";
+    formularioAgregar.ingredientes.value = "";
+    formularioAgregar.price.value = "";
+    alert("Platillo agregado");
+});
+
+const platilloBorrar = document.querySelector(".recipes");
+
+platilloBorrar.addEventListener("click", (e) => {
+    if (e.target.tagName === 'I') {
+
+        const confirmacion = confirm("¿Desea eliminar el platillo?");
+        if (confirmacion) {
+            const id = e.target.getAttribute("data-id");
+            db.collection("platillos").doc(id).delete()
+            .then(() => {
+                console.log("Platillo eliminado correctamente de la base de datos.");
+            })
+            .catch((error) => {
+                console.error("Error al eliminar: ", error);
+                alert("Hubo un error al intentar eliminar el platillo.");
+            });
+        }
+    }
 });
--- js/index.js
+++ js/index.js
@@ -11,15 +11,41 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function mostrarPlatillo(platillo, id) {
   contenido = `
-    <div class='card-panel recipe white row'>
+    <div class='card-panel recipe white row' id='${id}' data-id='${id}'>
         <div class='recipe-details'>
 
           <div class='recipe-title'>${platillo.nombre}</div>
           <div class='recipe-ingredients'>${platillo.ingredientes}</div>
+          <div class='recipe-precio'>Precio: $${platillo.precio} MXN</div>
 
+          <div class="recipe-delete">
+            <i class="material-icons" data-id='${id}'>
+            delete_outline
+            </i>
+          </div>
 
         </div>
     </div>
   `;
     document.querySelector(".recipes").innerHTML += contenido;
 };
+
+function actualizarPlatillo(platillo,id){
+  let tarjeta = document.getElementById(`${id}`);
+  tarjeta.querySelector(".recipe-title").innerHTML = platillo.nombre;
+  tarjeta.querySelector(".recipe-ingredientes").innerHTML = platillo.ingredientes;
+  tarjeta.querySelector(".recipe-precio").innerHTML = platillo.precio;
+}
+
+const borrarPlatillo = (id) =>{
+  const platillo = document.querySelector(`.recipe[data-id=${id}]`);
+  platillo.remove();
+};
+
+function agregarALista(platillo, id) {
+  contenidoLista = `
+  <option value="${id}">${platillo}</option>
+  `;
+
+  document.getElementById("listaPlatillos").innerHTML += contenidoLista;
+}
